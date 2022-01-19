d75e633b8554edd5baf6a90b83c48b6386b525ce
Simi Silvester Correia <66421852+simicorreia24@users.noreply.github.com>
2022-01-19 15:41:25 +0530

    Add files via upload

---
+++ Statistics Assign 5.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# STATISTICS ASSIGNMENT NO. 5
+# 
+# Question 1
+#  We throw two dice. Let A be the event “the sum of the points is 7”, B the event “die #1
+# came up 3”, and C the event “die #2 came up 4”. Now, P[A] = P[B] = P[C] = 1/6.
+# Also,
+# P[A ∩ B] = P[A ∩ C] = P[B ∩ C] = 1/36
+# so that all events are pairwise independent. However,
+# P[A ∩ B ∩ C] = P[B ∩ C] = 1/36
+# while
+# P[A]P[B]P[C] = 1/216
+# so they are not independent as a triplet.
+# First, note that, indeed, P[A ∩ B] = P[B ∩ C] = 1/36, since the fact that A and B occurred is the same as the fact that B and C occurred.
+# 
+# Question 2.
+#  Let G represents green marble and B represents blue marble. 
+# According to the question, after putting a green marble in the bag,  Let already present marble in the bag is green. Then, after taking green marble. 
+# Probability that the remaining marble is also green =1
+#  Let already present marble in the bag is blue, then after taking green marble. 
+# Probability that the remaining marble is also green = 0
+#  So, required probability =0+1=1
+# 
